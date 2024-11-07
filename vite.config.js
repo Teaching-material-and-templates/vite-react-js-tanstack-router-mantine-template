4e298a0c16808ea21a0a373210580ebe4bd84185
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-07 14:32:21 +0100

    removed base from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vite-react-js-tanstack-router-mantine-template/",
   plugins: [TanStackRouterVite(), react()],
 });

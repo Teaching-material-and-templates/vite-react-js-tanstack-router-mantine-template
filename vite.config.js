a3029d8de8af3d679cd0b1997a5f0bcd2e786db5
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-07 14:24:57 +0100

    trying to deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vite-react-js-tanstack-router-mantine-template/",
   plugins: [TanStackRouterVite(), react()],
 });

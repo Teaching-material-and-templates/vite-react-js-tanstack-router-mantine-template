39d93d24bf2b8beeaf3365bbb9e8ffac2c2ea540
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-07 13:16:14 +0100

    another example added for use of context

--- src/routes/about.lazy.jsx
+++ src/routes/about.lazy.jsx
@@ -1,9 +1,12 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, useRouteContext } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/about")({
   component: About,
 });
 
 function About() {
+  const context = useRouteContext({ from: "/about" });
+  console.log(context);
+
   return <div>Hello from About!</div>;
 }

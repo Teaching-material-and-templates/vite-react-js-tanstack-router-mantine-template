5135998ebd08c6fc319d50614c8b26b7f7ad2513
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-19 09:58:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 // Mantine Providers
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
+import { getSupabaseClient } from "./supabase/getSupabaseClient";
 // Router imports
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 // Generated Routetree
 import { routeTree } from "./routeTree.gen";
-import { getSupabaseClient } from "./supabase/getSupabaseClient";
 
 export const router = createRouter({
   routeTree,
